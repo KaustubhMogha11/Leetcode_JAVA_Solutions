0ba7bd0e5dba402c7b2b21eb2a564a6e5d173239
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-01-20 02:44:30 -0800

    Time: 4 ms (8.71%), Space: 39 MB (11.74%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -1,25 +1,37 @@
 class Solution {
     public int minAddToMakeValid(String s) {
 
-        char ch[]=s.toCharArray();
-        Stack<Character> st=new Stack<>();
-        for(int i=0; i<ch.length;i++)
-        {
-            if(ch[i]=='(')
-            {
-                st.push(ch[i]);
-                }
-            else if(!st.isEmpty() && st.peek()=='('){
-                    st.pop();
-            }else{
-                st.push(ch[i]);
+//         char ch[]=s.toCharArray();
+//         Stack<Character> st=new Stack<>();
+//         for(int i=0; i<ch.length;i++)
+//         {
+//             if(ch[i]=='(')
+//             {
+//                 st.push(ch[i]);
+//                 }
+//             else if(!st.isEmpty() && st.peek()=='('){
+//                     st.pop();
+//             }else{
+//                 st.push(ch[i]);
+//             }
+//         }
+//             int count=0;
+//             for(int j=0; j<st.size(); j++)
+//             {count++;
+//                 }
+        
+//         return count;
+//     }
+         Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if(c == '(') stack.push('(');
+            else {
+                if(!stack.isEmpty() && stack.peek() == '(' && c == ')')
+                    stack.pop();
+                else stack.push(')');
             }
         }
-            int count=0;
-            for(int j=0; j<st.size(); j++)
-            {count++;
-                }
-        
-        return count;
+        return stack.size();
     }
 }
