faba827c9091a27daa8cdf212c6a0334d3be745a
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-01-25 01:06:46 -0800

    Time: 0 ms (100.00%), Space: 36.8 MB (80.62%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -8,30 +8,41 @@ class MyQueue {
     }
     
     public void push(int x) {
-       while(st.size()>0)
-       {
-           helper.push(st.pop());
-       }st.push(x);
-        while(helper.size()>0)
-        {
-            st.push(helper.pop());
-        }
+       // while(st.size()>0)
+       // {
+       //     helper.push(st.pop());
+       // }
+        st.push(x);
+        // while(helper.size()>0)
+        // {
+        //     st.push(helper.pop());
+        // }
         
     }
     
     public int pop() {
-      return st.pop();
+      // return st.pop();
+        pushToStack2();
+        return helper.pop();
     }
     
+    
     public int peek() {
-        return st.peek();
+        pushToStack2();
+        return helper.peek();
     }
     
     public boolean empty() {
-        return st.size()==0;
+        return st.size()==0 && helper.size()==0;
+    }
+    public void pushToStack2() {
+        if(helper.empty()) {
+            while(!st.empty()) {
+                helper.push(st.pop());
+            }
+}
     }
 }
-
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue obj = new MyQueue();
