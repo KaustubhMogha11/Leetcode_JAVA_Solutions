74062a22139eb3d49f67cca6f39cbf255cf8e30d
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-02-09 01:05:49 -0800

    Time: 0 ms (100.00%), Space: 40.7 MB (12.35%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -2,31 +2,43 @@ class Solution {
     
      public int climbStairs(int n){
          
-         int storage[]=new int[n+1];
-         return  climbStairsM(n,storage);
+//          int storage[]=new int[n+1];
+//          return  climbStairsM(n,storage);
+         
+         int dp[]=new int [n+1];
+         dp[0]=1;
+         dp[1]=1;
+         
+         for(int i=2; i<=n; i++)
+         {
+             dp[i]=dp[i-1]+dp[i-2];
+             
+         }
+         
+         return dp[n];
      }
     
     
-    public int climbStairsM(int n, int[] storage) {
+//     public int climbStairsM(int n, int[] storage) {
     
-        if(n<0)
-        {
-            return 0;
-        }
+//         if(n<0)
+//         {
+//             return 0;
+//         }
         
-        if(n==0)
-        {
-            storage[n]=1;
-        return storage[n];
-        }
+//         if(n==0)
+//         {
+//             storage[n]=1;
+//         return storage[n];
+//         }
         
-        if(storage[n]!=0)
-        {
-            return storage[n];
-        }
-        storage[n]=climbStairsM(n-1, storage)+climbStairsM(n-2, storage);
+//         if(storage[n]!=0)
+//         {
+//             return storage[n];
+//         }
+//         storage[n]=climbStairsM(n-1, storage)+climbStairsM(n-2, storage);
         
-        return storage[n];
-    }
+//         return storage[n];
+//     }
     
 }
