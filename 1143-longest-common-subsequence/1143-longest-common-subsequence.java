22bf3444be1a18c1ff7936182fd50e2a974efe90
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-02-14 09:23:14 -0800

    Time: 9 ms (92.13%), Space: 45.9 MB (28.48%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,6 +3,8 @@ class Solution {
      int storage[][]=new int [text1.length()+1][text2.length()+1];
         return longestCommonSubsequence(text1,text2, storage);
         
+        
+        
     }
     public int longestCommonSubsequence(String text1, String text2, int[][] storage) {
         
@@ -11,25 +13,50 @@ class Solution {
         
         if(m==0 || n==0)
         {
-            return 0;
-        }
-        
-        if(storage[m][n]!=0)
-        {
+            storage[m][n]=0;
+            
             return storage[m][n];
         }
         
-        if(text1.charAt(0)==text2.charAt(0))
+        for(int i=0; i<=m;i++)
         {
-            storage[m][n]=1+longestCommonSubsequence(text1.substring(1),text2.substring(1),storage);
-            
-        }else{
-            int option1=longestCommonSubsequence(text1,text2.substring(1),storage);
-             int option2=longestCommonSubsequence(text1.substring(1),text2,storage);
-         
-            storage[m][n]=Math.max(option1,option2);
-        
+            storage[i][0]=0;
         }
+        
+         for(int i=0; i<=n;i++)
+        {
+            storage[0][i]=0;
+        }
+        
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(text1.charAt(m-i)==text2.charAt(n-j))
+                {
+                    storage[i][j]=1+storage[i-1][j-1];
+                }else{
+                     storage[i][j]=Math.max(storage[i-1][j],storage[i][j-1]);
+                }
+            }
+        }
+        
+        // if(storage[m][n]!=0)
+        // {
+        //     return storage[m][n];
+        // }
+        
+//         if(text1.charAt(0)==text2.charAt(0))
+//         {
+//             storage[m][n]=1+longestCommonSubsequence(text1.substring(1),text2.substring(1),storage);
+            
+//         }else{
+//             int option1=longestCommonSubsequence(text1,text2.substring(1),storage);
+//              int option2=longestCommonSubsequence(text1.substring(1),text2,storage);
+         
+//             storage[m][n]=Math.max(option1,option2);
+        
+//         }
         
         return storage[m][n];
         
