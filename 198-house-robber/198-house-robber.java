a10daef8e97dd25e93c15845b4e813a5cf90b616
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-02-17 09:54:38 -0800

    Time: 0 ms (100.00%), Space: 39.3 MB (33.49%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,19 +1,36 @@
 class Solution {
     public int rob(int[] nums) {
-        int dp[]=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return getMaximumSum(nums.length-1,nums,dp);
-    }
-    public int getMaximumSum(int index,int [] nums,int []dp)
-    {
-        if(index==0)return nums[index];
-        if(index<0)return 0;
-        if(dp[index]!=-1)return dp[index];
-        int pick=nums[index]+ getMaximumSum(index-2,nums,dp);
-        int notPick=getMaximumSum(index-1,nums,dp);
-        dp[index]=Math.max(pick,notPick);
-        return dp[index];
-    }
+     // int dp[]=new int[nums.length];
+     //    Arrays.fill(dp,-1);
+     //    return getMaximumSum(nums.length-1,nums,dp);
+        
+        int n=nums.length;
+        int storage[]=new int[n+1];
+        if(n==0)
+        {
+            return 0;
+        }
+        storage[0]=0;
+        storage[1]=nums[0];
+        
+        for(int i=2;i<=n;i++){
+            storage[i]=Math.max(storage[i-1], nums[i-1]+storage[i-2]);
+        }
+        
+        return storage[n];
+        
+    }   
+//     public int getMaximumSum(int index,int [] nums,int []dp)
+//     {
+//         // if(index==0)return nums[index];
+//         // if(index<0)return 0;
+//         // if(dp[index]!=-1)return dp[index];
+//         // int pick=nums[index]+ getMaximumSum(index-2,nums,dp);
+//         // int notPick=getMaximumSum(index-1,nums,dp);
+//         // dp[index]=Math.max(pick,notPick);
+//         // return dp[index];
+        
+//     }
         
 }
 
