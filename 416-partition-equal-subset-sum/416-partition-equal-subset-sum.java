f8f6b1c5c39e54d9ab33586e9d839c6ac3f3bdac
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-02-24 08:41:06 -0800

    Time: 5 ms (98.15%), Space: 55.9 MB (32.21%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,21 +1,32 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        
         int sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
+        
+        for(int i=0; i<nums.length;i++)
+        {
+            sum=sum+nums[i];
         }
-        if(sum%2==1)
+        
+        if(sum%2==1){
             return false;
-        Boolean dp[][]=new Boolean[nums.length+1][(sum/2)+1];
-        return partition(nums,sum/2,0,dp);
+        }
+        
+        Boolean storage[][]=new Boolean[nums.length+1][(sum/2)+1];
+        
+        return canPartitionm(nums,0,sum/2,storage);
     }
-    public boolean partition(int a[],int sum,int i,Boolean dp[][]){
-        if(sum==0)
+     public boolean canPartitionm(int[] nums, int i, int sum, Boolean storage[][]){
+         
+       if(sum==0)
             return true;
-		if(i>=a.length || sum<0)
+		if(i>=nums.length || sum<0)
             return false;
-        if(dp[i][sum]!=null)
-            return dp[i][sum];
-        return dp[i][sum]=(partition(a,sum-a[i],i+1,dp) || partition(a,sum,i+1,dp));
+         
+        if(storage[i][sum]!=null)
+            return storage[i][sum];
+         
+         
+        return storage[i][sum]=(canPartitionm(nums,i+1,sum-nums[i],storage) || canPartitionm(nums,i+1,sum,storage));
     }
 }
