0ba4b1793577cb4caab5ef05dfa2b666fcb34f63
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-02-27 01:22:51 -0800

    Time: 30 ms (82.61%), Space: 43 MB (72.33%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -8,25 +8,43 @@ class Solution {
             sum=sum+nums[i];
         }
         
-        if(sum%2==1){
+        if(sum%2!=0){
             return false;
+        }else{
+        
+        // Boolean storage[][]=new Boolean[nums.length+1][(sum/2)+1];
+        
+        // return canPartitionm(nums,0,sum/2,storage);
+         boolean[][] dp = new boolean[nums.length + 1][sum/2 +1];
+        
+        for(int i=0; i<dp.length; i++){
+            dp[i][0]=true;
         }
         
-        Boolean storage[][]=new Boolean[nums.length+1][(sum/2)+1];
+        for(int i=1; i<dp.length; i++){ //nums
+            for(int j=1; j<dp[0].length; j++){
+                if(nums[i-1]<=j){
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[nums.length][sum/2];
+        }
         
-        return canPartitionm(nums,0,sum/2,storage);
     }
-     public boolean canPartitionm(int[] nums, int i, int sum, Boolean storage[][]){
+//      public boolean canPartitionm(int[] nums, int i, int sum, Boolean storage[][]){
          
-       if(sum==0)
-            return true;
-		if(i>=nums.length || sum<0)
-            return false;
+//        if(sum==0)
+//             return true;
+// 		if(i>=nums.length || sum<0)
+//             return false;
          
-        if(storage[i][sum]!=null)
-            return storage[i][sum];
+//         if(storage[i][sum]!=null)
+//             return storage[i][sum];
          
          
-        return storage[i][sum]=(canPartitionm(nums,i+1,sum-nums[i],storage) || canPartitionm(nums,i+1,sum,storage));
-    }
+//         return storage[i][sum]=(canPartitionm(nums,i+1,sum-nums[i],storage) || canPartitionm(nums,i+1,sum,storage));
+//     }
 }
