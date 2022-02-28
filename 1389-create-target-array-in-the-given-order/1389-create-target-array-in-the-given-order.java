d60c8fb247b2b3474e337f0afe403eb69dc845c5
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-02-28 09:23:32 -0800

    Time: 1 ms (79.73%), Space: 40.3 MB (46.77%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,20 +1,38 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
         
-        ArrayList <Integer> list=new ArrayList<>();
+//         ArrayList <Integer> list=new ArrayList<>();
         int ans[]=new int[nums.length];
         
-        for(int i=0; i<nums.length;i++)
-        {
-            list.add(index[i], nums[i]);
+//         for(int i=0; i<nums.length;i++)
+//         {
+//             list.add(index[i], nums[i]);
             
+//         }
+        
+//         for(int j=0;j<list.size();j++){
+            
+//             ans[j]=list.get(j);
+//         }
+        
+//         return ans;
+        for(int i=0; i<nums.length;i++){
+            ans[i]=-1;
         }
         
-        for(int j=0;j<list.size();j++){
-            
-            ans[j]=list.get(j);
+        for(int i=0; i<nums.length;i++){
+            if(nums[i]==-1){
+                ans[index[i]]=nums[i];
+            }else{
+               // Shifting
+                for (int j = index[i] + 1; j < nums.length; j++){
+                    int temp = ans[j];
+                    ans[j] = ans[index[i]];
+                    ans[index[i]] = temp;
+                }
+                ans[index[i]] = nums[i];
+            }
         }
-        
         return ans;
     }
 }
