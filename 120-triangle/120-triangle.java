19710a6557e1fb13319345b412e941863be238a6
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-05-07 10:39:31 -0700

    Time: 1 ms (99.72%), Space: 42 MB (83.06%) - LeetHub

---
+++ 120-triangle/120-triangle.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        
+    Integer [][] storage=new Integer[triangle.size()][triangle.size()];
+     
+        return minimumTotal(triangle,0,0,storage);
+        
+    }
+       public int minimumTotal(List<List<Integer>> triangle,int row, int col, Integer[][] storage)
+       {
+           if(row==triangle.size()-1)
+           {
+                return triangle.get(row).get(col);
+           }
+           
+           if(storage[row][col]!=null)
+           {
+               return storage[row][col];
+           }
+            int opt1=minimumTotal(triangle,row+1,col,storage);
+            int opt2= minimumTotal(triangle,row+1,col+1,storage);
+        
+           storage[row][col]=triangle.get(row).get(col)+Math.min(opt1,opt2);
+           
+           return storage[row][col];
+       }
+    
+}
