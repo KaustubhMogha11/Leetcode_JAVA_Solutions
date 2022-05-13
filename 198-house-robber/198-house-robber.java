a781f7888480c6179554060cc22b086dd6777890
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-05-13 11:03:33 -0700

    Time: 0 ms (100.00%), Space: 39.7 MB (83.99%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,37 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-     // int dp[]=new int[nums.length];
-     //    Arrays.fill(dp,-1);
-     //    return getMaximumSum(nums.length-1,nums,dp);
-        
         int n=nums.length;
-        int storage[]=new int[n+1];
-        if(n==0)
+      int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=nums[0];
+        
+        for(int i=2;i<=nums.length;i++)
         {
-            return 0;
+            dp[i]=Math.max(dp[i-1],nums[i-1]+dp[i-2]);
         }
-        storage[0]=0;
-        storage[1]=nums[0];
-        
-        for(int i=2;i<=n;i++){
-            storage[i]=Math.max(storage[i-1], nums[i-1]+storage[i-2]);
+            
+          return dp[n];
         }
-        
-        return storage[n];
-        
-    }   
-//     public int getMaximumSum(int index,int [] nums,int []dp)
-//     {
-//         // if(index==0)return nums[index];
-//         // if(index<0)return 0;
-//         // if(dp[index]!=-1)return dp[index];
-//         // int pick=nums[index]+ getMaximumSum(index-2,nums,dp);
-//         // int notPick=getMaximumSum(index-1,nums,dp);
-//         // dp[index]=Math.max(pick,notPick);
-//         // return dp[index];
-        
-//     }
-        
 }
-
-
