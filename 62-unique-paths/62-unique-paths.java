8d71364b1e14ecb8464c7e3ada2318b95924befd
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-05-14 03:18:48 -0700

    Time: 0 ms (100.00%), Space: 39.2 MB (80.61%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,56 +1,34 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int storage[][]=new int[m+1][n+1];
      
-        
-        for(int i=1; i<=m;i++)
-        {
-            for(int j=1; j<=n; j++)
-            {
-                if(i==1 || j==1)
-                {
-                    storage[i][j]=1;
-                } else
-                {
-                
-                storage[i][j]= storage[i-1][j]+storage[i][j-1];
-            }
-        }
-        }      
-        
-        return storage[m][n];
-            // return uniquePathsm(m,n,0,0,storage);
-            
+        int dp[][]=new int[m+1][n+1];
+        return uniquePaths(m,n,0,0,dp);
     }
-//      public int uniquePathsm(int m, int n, int i, int j, int [][]storage){
+      public int uniquePaths(int m, int n,int i, int j,int dp[][]){
+          
+          if(dp[i][j]!=0)
+          {
+              return dp[i][j];
+          }
          
-//          if(i==m-1 && j==n-1)
-//          {
-//              // storage[m-1][n-1]=1;
-             
-//              return 1;
-//          }
-         
-//           if(i>=m || j>=n  ){
-//              return Integer.MAX_VALUE;
-//          }
-         
-//           if(storage[i][j]!=0)
-//          {
-//              return storage[i][j];
-//          }
-         
-//         int count=0;
-         
-//          if(i+1<=m-1)
-//          {
-//            count+=uniquePathsm(m,n,i+1,j, storage);
-//          }
-//          if(j+1<=n-1)
-//          {
-//             count+= uniquePathsm(m,n,i,j+1, storage);
-//          }
-//          storage[i][j]=count;
-//          return storage[i][j];
-//      }
+          if(i==m-1 && j==n-1)
+          {
+              dp[i][j]=1;
+              return dp[i][j];
+          }
+          
+          if(i>=m || j>=n)
+          {
+              dp[i][j]=0;
+              return dp[i][j];
+          }
+          
+          int left=uniquePaths(m,n,i,j+1,dp);
+           int right=uniquePaths(m,n,i+1,j,dp);
+          
+          dp[i][j]=left+right;
+          
+          return dp[i][j];
+      }
+    
 }
