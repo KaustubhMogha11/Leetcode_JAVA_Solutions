50a4fde9e4b3b3ed2db42d3f7020fb1507e05f13
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-05-16 02:54:29 -0700

    Time: 1 ms (51.77%), Space: 42 MB (63.75%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,47 +1,60 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        
-       int dp[][]=new int[obstacleGrid.length+1][obstacleGrid[0].length+1];
-        
-        return  uniquePathsWithObstacles(obstacleGrid,0,0,dp);
+     int dp[][] = new int[obstacleGrid.length+1][obstacleGrid[0].length+1];
+    for(int i=0;i<obstacleGrid.length;i++)
+    {
+        for(int j=0;j<obstacleGrid[0].length;j++)
+        {
+            if(i==0&&j==0&&obstacleGrid[i][j]==0)
+                dp[i+1][j+1] = 1;
+            else if(obstacleGrid[i][j]==0)
+                dp[i+1][j+1] = dp[i][j+1]+dp[i+1][j];
+        }
     }
-      public int uniquePathsWithObstacles(int[][] grid,int i, int j, int[][] dp)
-      {
-          int m=grid.length;
-          int n=grid[0].length;
+   
+    return dp[obstacleGrid.length][obstacleGrid[0].length];
+
+        
+    }
+//         return  uniquePathsWithObstacles(obstacleGrid,0,0,dp);
+//     }
+//       public int uniquePathsWithObstacles(int[][] grid,int i, int j, int[][] dp)
+//       {
+//           int m=grid.length;
+//           int n=grid[0].length;
          
           
-          if(dp[i][j]!=0)
-          {
-              return dp[i][j];
-          }
-           if(i>=m || j>=n)
-          {
-              dp[i][j]=0;
-              return dp[i][j];
+//           if(dp[i][j]!=0)
+//           {
+//               return dp[i][j];
+//           }
+//            if(i>=m || j>=n)
+//           {
+//               dp[i][j]=0;
+//               return dp[i][j];
               
-          }
-            if(grid[i][j]==1)
-          {
-              return 0;
-          }
+//           }
+//             if(grid[i][j]==1)
+//           {
+//               return 0;
+//           }
           
-          if(i==m-1 && j==n-1)
-          {
-              dp[i][j]=1;
-              return dp[i][j];
+//           if(i==m-1 && j==n-1)
+//           {
+//               dp[i][j]=1;
+//               return dp[i][j];
               
-          }
+//           }
           
          
          
               
-          int left=uniquePathsWithObstacles(grid,i+1,j,dp);
-          int right=uniquePathsWithObstacles(grid,i,j+1,dp);
+//           int left=uniquePathsWithObstacles(grid,i+1,j,dp);
+//           int right=uniquePathsWithObstacles(grid,i,j+1,dp);
           
-          dp[i][j]=left+right;
+//           dp[i][j]=left+right;
           
-          return dp[i][j];
-      }
+//           return dp[i][j];
+//       }
     
 }
