2d20da70ac654b6801169545a20a678968170321
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-05-20 01:55:53 -0700

    Time: 1 ms (52.25%), Space: 42.2 MB (54.36%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,20 +1,37 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-     int dp[][] = new int[obstacleGrid.length+1][obstacleGrid[0].length+1];
-    for(int i=0;i<obstacleGrid.length;i++)
-    {
-        for(int j=0;j<obstacleGrid[0].length;j++)
-        {
-            if(i==0&&j==0&&obstacleGrid[i][j]==0)
-                dp[i+1][j+1] = 1;
-            else if(obstacleGrid[i][j]==0)
-                dp[i+1][j+1] = dp[i][j+1]+dp[i+1][j];
-        }
-    }
+//      int dp[][] = new int[obstacleGrid.length+1][obstacleGrid[0].length+1];
+//     for(int i=0;i<obstacleGrid.length;i++)
+//     {
+//         for(int j=0;j<obstacleGrid[0].length;j++)
+//         {
+//             if(i==0&&j==0&&obstacleGrid[i][j]==0)
+//                 dp[i+1][j+1] = 1;
+//             else if(obstacleGrid[i][j]==0)
+//                 dp[i+1][j+1] = dp[i][j+1]+dp[i+1][j];
+//         }
+//     }
    
-    return dp[obstacleGrid.length][obstacleGrid[0].length];
-
+//     return dp[obstacleGrid.length][obstacleGrid[0].length];
+  
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
         
+        int dp[][]=new int[m+1][n+1];
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 & j==0 && obstacleGrid[i][j]==0)
+                {
+                    dp[i+1][j+1]=1;
+                        
+                }else if(obstacleGrid[i][j]==0)
+                dp[i+1][j+1] = dp[i][j+1]+dp[i+1][j];
+            }
+        }
+        return dp[m][n];
     }
 //         return  uniquePathsWithObstacles(obstacleGrid,0,0,dp);
 //     }
