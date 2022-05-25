90f9c40188c60768f64fbba633fa664a0b1e740b
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-05-25 04:26:35 -0700

    Time: 701 ms (17.38%), Space: 41.8 MB (74.91%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,25 +1,18 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-     int sum = 0;
-        for(int x : nums)
-            sum += x;
-        if(((sum - target) % 2 == 1) || (target > sum))
-            return 0;
-        
-        int n = nums.length;
-        int s2 = (sum - target)/2;
-        int[][] t = new int[n + 1][s2 + 1];
-        t[0][0] = 1;
-        
-        for(int i = 1; i < n + 1; i++) {
-            for(int j = 0; j < s2 + 1; j++) {
-                if(nums[i - 1] <= j)
-                    t[i][j] = t[i-1][j] + t[i - 1][j - nums[i - 1]];
-                else
-                    t[i][j] = t[i - 1][j];
-            }
-        }
-        return t[n][s2];
-        }
-        
-    }
+   int ans = 0;
+
+public int findTargetSumWays(int[] nums, int target) {
+	backtrack(0, -target, nums);
+	return ans;
+}
+
+private void backtrack(int i, int sum, int[] nums) {
+	if (i == nums.length) {
+		if (sum == 0) ans++;
+		return;
+	}
+
+	backtrack(i + 1, sum + nums[i], nums);
+	backtrack(i + 1, sum - nums[i], nums);
+}
+}
