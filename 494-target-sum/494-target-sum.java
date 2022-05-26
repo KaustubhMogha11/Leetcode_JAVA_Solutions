62687fcc7c3d58d7890a8f1a4ca8170daa5f3acf
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-05-26 02:23:42 -0700

    Time: 4 ms (95.39%), Space: 41.7 MB (78.10%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,18 +1,30 @@
 class Solution {
-   int ans = 0;
-
-public int findTargetSumWays(int[] nums, int target) {
-	backtrack(0, -target, nums);
-	return ans;
-}
-
-private void backtrack(int i, int sum, int[] nums) {
-	if (i == nums.length) {
-		if (sum == 0) ans++;
-		return;
-	}
-
-	backtrack(i + 1, sum + nums[i], nums);
-	backtrack(i + 1, sum - nums[i], nums);
-}
+    public int findTargetSumWays(int[] nums, int target) {
+        
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum=sum+nums[i];
+        }
+         if(((sum - target) % 2 == 1) || (target > sum))
+            return 0;
+      int s1=(sum-target)/2;
+        int[][] dp=new int[nums.length+1][s1+1];
+    dp[0][0]=1;
+        
+        for(int i=1;i<nums.length+1;i++)
+        {
+            for(int j=0;j<s1+1;j++)
+            {
+                if(nums[i-1]<=j)
+                {
+                    dp[i][j]=dp[i-1][j-nums[i-1]]+dp[i-1][j];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[nums.length][s1];
+       }
 }
