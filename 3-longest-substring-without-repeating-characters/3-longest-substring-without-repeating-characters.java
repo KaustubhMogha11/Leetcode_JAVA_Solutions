07b5bdd393f0ff0cf9e4a96220e1e8d40aff150a
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-06-10 11:02:59 -0700

    Time: 19 ms (26.74%), Space: 44.2 MB (62.61%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,21 +1,62 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         
+//         int i=0;
+//         int j=0;
+//         int max=0;
+//         HashMap<Character,Integer> map=new HashMap<Character,Integer>();
+        
+//         while(j<s.length())
+//         {
+//             char c=s.charAt(j);
+//             if(map.containsKey(c))
+//                 map.put(c,map.get(c)+1);
+//                 else map.put(c,1);
+            
+//             if(map.size()==j-i+1)
+//             {
+//                 max=Math.max(max,j-i+1);
+//             }
+            
+//             if(map.size()<j-i+1)
+//             {
+//                 while(map.size()<j-i+1)
+//                 {
+//                     char t=s.charAt(i);
+//                     if(map.containsKey(t))
+//                     {
+//                         map.put(t,map.get(t)-1);
+//                     }
+                    
+//                     if(map.get(t)==0)
+//                     {
+//                         map.remove(t);
+//                     }
+                    
+//                     i++;
+//                 }
+               
+//             }
+//             j++;
+//         }
+        
+//         return max;
+        
         int i=0;
         int j=0;
         int max=0;
-        HashMap<Character,Integer> map=new HashMap<Character,Integer>();
-        
+        HashMap<Character,Integer> map=new HashMap<>();
         while(j<s.length())
         {
-            char c=s.charAt(j);
-            if(map.containsKey(c))
-                map.put(c,map.get(c)+1);
-                else map.put(c,1);
-            
+            if(map.containsKey(s.charAt(j)))
+            {
+                map.put(s.charAt(j),map.get(s.charAt(j))+1);
+            }else{
+                map.put(s.charAt(j),1);
+            }
             if(map.size()==j-i+1)
             {
-                max=Math.max(max,j-i+1);
+                max=Math.max(j-i+1,max);
             }
             
             if(map.size()<j-i+1)
@@ -35,7 +76,6 @@ class Solution {
                     
                     i++;
                 }
-               
             }
             j++;
         }
