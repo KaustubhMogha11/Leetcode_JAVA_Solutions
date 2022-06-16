5bce827443001566c873b50ecf484bef47b799d1
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-06-16 03:24:21 -0700

    Time: 259 ms (30.05%), Space: 128.2 MB (54.94%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -1,19 +1,36 @@
 class NumMatrix {
-    private int[][] sums;
-    
+
+    int [][] matrix;
     public NumMatrix(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        sums = new int[n+1][m+1];
-        for (int i=1; i<=n; i++)
-            for (int j=1; j<=m; j++)
-                sums[i][j] = sums[i-1][j] + sums[i][j-1] + matrix[i-1][j-1] - sums[i-1][j-1];
+        
+        for(int i=1;i<matrix.length;i++)
+        {
+            for( int j=0;j<matrix[0].length;j++)
+            {
+                matrix[i][j]+=matrix[i-1][j];
+            }
+        }
+         for(int i=0;i<matrix.length;i++)
+        {
+            for( int j=1;j<matrix[0].length;j++)
+            {
+                matrix[i][j]+=matrix[i][j-1];
+            }
+        }
+        this.matrix=matrix;
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int RED_RECTANGLE = sums[row2+1][col2+1];
-        int PURPLE_RECTANGLES = sums[row1][col2+1] + sums[row2+1][col1];
-        int BLUE_RECTANGLE = sums[row1][col1];
-        return RED_RECTANGLE - PURPLE_RECTANGLES + BLUE_RECTANGLE;
+        int total=matrix[row2][col2];
+        int extra=(row1!=0?matrix[row1-1][col2]:0)+(col1!=0?matrix[row2][col1-1]:0)-((row1!=0 && col1!=0)?matrix[row1-1][col1-1]:0);
+        
+        return total-extra;
+        
     }
 }
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
