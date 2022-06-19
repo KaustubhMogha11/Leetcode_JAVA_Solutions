f0ad2783ffa9fb2d9981be3c0103a6f0e5e9927f
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-06-19 04:53:57 -0700

    Time: 15 ms (37.22%), Space: 44.6 MB (57.67%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -42,9 +42,12 @@ class Solution {
         
 //         return max;
         
-        int i=0;
+        
+        
+    int i=0;
         int j=0;
         int max=0;
+        
         HashMap<Character,Integer> map=new HashMap<>();
         while(j<s.length())
         {
@@ -54,27 +57,24 @@ class Solution {
             }else{
                 map.put(s.charAt(j),1);
             }
+            
             if(map.size()==j-i+1)
             {
-                max=Math.max(j-i+1,max);
-            }
-            
-            if(map.size()<j-i+1)
-            {
-                while(map.size()<j-i+1)
+                max=Math.max(max,j-i+1);
+            }else{
+                while(map.size()!=j-i+1)
                 {
-                    char t=s.charAt(i);
-                    if(map.containsKey(t))
+                    if(map.containsKey(s.charAt(i)))
                     {
-                        map.put(t,map.get(t)-1);
+                        map.put(s.charAt(i),map.get(s.charAt(i))-1);
                     }
-                    
-                    if(map.get(t)==0)
+                    if(map.get(s.charAt(i))==0)
                     {
-                        map.remove(t);
+                        map.remove(s.charAt(i));
                     }
                     
                     i++;
+                    max=Math.max(max,j-i+1);
                 }
             }
             j++;
