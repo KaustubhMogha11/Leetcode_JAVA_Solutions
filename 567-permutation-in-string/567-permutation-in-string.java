e6656a5e777d567767437fa693790bad965dfbf3
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-06-21 04:03:13 -0700

    Time: 33 ms (33.55%), Space: 43.1 MB (62.71%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,20 +1,67 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-      ArrayList<Integer> list=new ArrayList<>();
-        HashMap<Character, Integer> map=new HashMap<>();
+//       ArrayList<Integer> list=new ArrayList<>();
+//         HashMap<Character, Integer> map=new HashMap<>();
+//         int i=0;
+//         int j=0;
+//         int k=s1.length();
+       
+        
+//           for(int l=0;l<s1.length();l++){
+//             char character=p.charAt(l);
+//             map.put(character,map.getOrDefault(character,0)+1);
+              
+//         }
+        
+//          int count=0;
+//         while(j<s.length())
+//         {
+//            char character=s.charAt(j);
+//             if(map.containsKey(character)){
+//                 map.put(character,map.get(character)-1);
+//                 if(map.get(character)==0) 
+//                     count++;
+//             }
+            
+//             if(j-i+1<k)
+//             {
+//                 j++;
+//             }else if(j-i+1==k)
+//             {
+//                if(count==map.size()) 
+//                {
+//                    return true;
+                
+//             }
+//                char new_character=s.charAt(i);
+                
+//                 if(map.containsKey(new_character)){
+                    
+//                     if(map.get(new_character)==0) 
+//                         count--;
+//                     map.put(new_character,map.get(new_character)+1);
+//                 }
+            
+          
+//                 i++;
+//                 j++;
+//             }
+//         }
+        
+//         return false;
+        
+        HashMap<Character,Integer> map=new HashMap<>();
+        int count=0;
         int i=0;
         int j=0;
         int k=s1.length();
-       
-        
-          for(int l=0;l<s1.length();l++){
+        for(int l=0;l<s1.length();l++)
+        {
             char character=s1.charAt(l);
             map.put(character,map.getOrDefault(character,0)+1);
-              
         }
         
-         int count=0;
-        while(j<s2.length())
+    while(j<s2.length())
         {
            char character=s2.charAt(j);
             if(map.containsKey(character)){
