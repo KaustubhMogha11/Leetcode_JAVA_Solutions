95c5a152603db83f3df09fae63990e5ff0e43281
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-06-24 09:09:06 -0700

    Time: 2 ms (73.06%), Space: 44.6 MB (59.89%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -29,10 +29,27 @@ class Solution {
     
           public static void permutations(int[] nums, List<List<Integer>> ans, int index)
           {
+//               if(index==nums.length)
+//               {
+//                    List<Integer> ds=new ArrayList<>();
+//                   for(int i=0; i<nums.length;i++)
+//                   {
+//                       ds.add(nums[i]);
+//                   }
+//                   ans.add(new ArrayList<>(ds));
+//               }
+              
+//               for(int i=index;i<nums.length;i++)
+//               {
+//                   swap(i,index,nums);
+//                    permutations(nums,ans,index+1);
+//                    swap(i,index,nums);
+//               }
+              
               if(index==nums.length)
               {
-                   List<Integer> ds=new ArrayList<>();
-                  for(int i=0; i<nums.length;i++)
+                  List<Integer> ds=new ArrayList<>();
+                  for(int i=0;i<nums.length;i++)
                   {
                       ds.add(nums[i]);
                   }
