bb7d51207549824c81fd09f4dc454a747525617d
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-06-25 02:11:32 -0700

    Time: 0 ms (100.00%), Space: 42.2 MB (10.20%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,19 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
-//         int n=nums.length;
-//       int dp[]=new int[n+1];
-//         dp[0]=0;
-//         dp[1]=nums[0];
-        
-//         for(int i=2;i<=nums.length;i++)
-//         {
-//             dp[i]=Math.max(dp[i-1],nums[i-1]+dp[i-2]);
-//         }
-            
-//           return dp[n];
-//         }
-        
-        int dp[]=new int[nums.length+1];
+
+         int dp[]=new int[nums.length+1];
         dp[0]=0;
         dp[1]=nums[0];
         
@@ -23,5 +11,6 @@ class Solution {
         }
         
         return dp[nums.length];
+        
 }
 }
