ae95543c217d625578b3663e462b1abb2e7e52a5
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-06-28 03:32:13 -0700

    Time: 0 ms (100.00%), Space: 42.7 MB (68.98%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,46 +9,48 @@
  * }
  */
 class Solution {
-        public ListNode reverseList(ListNode head) {
-        
-//             return reverseList(head, null);
+    public ListNode reverseList(ListNode head) {
+       //recursive 
+//         if(head==null || head.next==null)
+//         {
+//             return head;
 //         }
-//     public ListNode reverseList(ListNode head,ListNode prev ) {
-      
-//         if(head==null)
-//             return prev;
-//         ListNode newhead=head.next;
-//         head.next=prev;
-//         prev=head;
-//        return  reverseList(newhead, prev);
-            if(head==null || head.next==null)
-                return head;
-            //  ListNode newhead=reverseList(head.next);
-            //  ListNode temp=newhead;
-            // while(temp.next!=null)
-            // {
-            //     temp=temp.next;
-            // }
-            // temp.next=head;
-            // head.next=null;
-            // return newhead;
-            
-            // ListNode newtail=head.next;
-            // ListNode newhead=reverseList(head.next);
-            // newtail.next=head;
-            // head.next=null;
-            // return newhead;
-            ListNode curr=head;
-            ListNode prev=null;
-            ListNode temp;
-            while(curr!=null)
-            {
-                temp=curr.next;
-                curr.next=prev;
-                prev=curr;
-                curr=temp;
-            }
-            return prev;
+        
+//         ListNode finalHead=reverseList(head.next);
+//         ListNode temp=finalHead;
+//         while(temp.next!=null)
+//         {
+//             temp=temp.next;
+//         }
+//         temp.next=head;
+//         head.next=null;
+        
+//         return finalHead;
+        
+        if(head==null || head.next==null)
+        {
+            return head;
+        }
+        
+//         ListNode tail=head.next;
+//         ListNode temp=reverseList(head.next);
+//         tail.next=head;
+//         head.next=null;
+        
+//         return temp;
+        ListNode temp;
+        ListNode curr=head;
+        ListNode prev=null;
+        
+        while(curr!=null)
+        {
+            temp=curr.next;
+            curr.next=prev;
+             prev=curr;
+            curr=temp;
             
         }
+        
+        return prev;
+    }
 }
