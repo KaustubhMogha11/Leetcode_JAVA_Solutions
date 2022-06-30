8f1ac2350393e34fa57d251c210a73fb15b75d27
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-06-30 09:19:41 -0700

    Time: 0 ms (100.00%), Space: 41.5 MB (13.71%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,45 +1,33 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-   
-        int dp[][]=new int[m+1][n+1];
         
-        for(int i=1;i<=m;i++)
-            for(int j=1;j<=n;j++)
-            {
-                if(i==1 || j==1)
-                    dp[i][j]=1;
-                else
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
+        int dp[][]=new int[m][n];
         
-        return dp[m][n];
-       
-//         int dp[][]=new int[m][n];
-        
-//         return uniquePaths(m,n,0,0,dp);
-//       }
+        return uniquePaths(m-1,n-1,dp);
+    }
     
-//       public int uniquePaths(int m, int n, int i, int j, int[][] dp) {
+      public int uniquePaths(int m, int n,int[][] dp) {
           
-//           if(i>=m || j>=n)
-//           {
-//               return 0;
-//           }
-//           if(i==m-1 || j==n-1)
-//           {
-//               return 1;
-//           }
+       if(m==0 && n==0)
+       {
+           return 1;
+       }
+          if(m<0 || n<0)
+          {
+              return 0;
+          }
           
-//           if(dp[i][j]!=0)
-//           {
-//               return dp[i][j];
-//           }
+          if(dp[m][n]!=0)
+          {
+              return dp[m][n];
+          }
           
-//           int right= uniquePaths(m,n,i,j+1,dp);
-//           int down= uniquePaths(m,n,i+1,j,dp);
+        int down= uniquePaths(m-1,n,dp);
+          int right= uniquePaths(m,n-1,dp);
           
-//           return dp[i][j]=right+down;
+          return dp[m][n]=down+right;
+          
+          
           
       }
-    
 }
