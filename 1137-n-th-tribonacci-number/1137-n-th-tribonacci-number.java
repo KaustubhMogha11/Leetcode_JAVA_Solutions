2b41294cac96d1df6c1afd7d43f97a9143ce9ac5
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-07-03 03:13:31 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (49.16%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,22 +1,24 @@
 class Solution {
     public int tribonacci(int n) {
-       if(n == 0) {
+        
+        if(n==0)
+        {
             return 0;
         }
-        if(n == 1 || n == 2) {
+        if(n==1 || n==2)
+        {
             return 1;
-        }  
+        }
         
-            int t0 = 0, t1 = 1, t2 = 1, answer = 0;
-            for(int i=3;i<=n;i++) {
-                answer = t0 + t1 + t2;
-                t0 = t1;
-                t1 = t2;
-                t2 = answer;
-            }
-                
-                
-            return answer;
+    int t1=0,t2=1,t3=1,ans=0;
+        for(int i=3;i<=n;i++)
+        {
+            ans=t1+t2+t3;
+            t1=t2;
+            t2=t3;
+            t3=ans;
+        }
         
+        return ans;
     }
 }
