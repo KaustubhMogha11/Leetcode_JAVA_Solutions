055744c5ccb95ca88ca4f419f69be6fde89fc3f1
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-07-10 10:52:19 -0700

    Time: 1 ms (89.45%), Space: 42.1 MB (89.01%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,29 +1,38 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         
-        int dp[]=new int[cost.length];
-        return Math.min(min(cost,0,dp),min(cost,1,dp));
+       int n=cost.length;
+        int dp[]=new int[n];
+//         dp[0]=0;
+//         dp[1]=0;
+        
+//         for(int i=2;i<n+1;i++)
+//         {
+//             dp[i]=Math.min(dp[i-2]+cost[i-1],dp[i-1]);
+//         }
+        
+//         return dp[n];
+        return Math.min(ans(cost,dp,0),ans(cost,dp,1));
     }
-    
-    public int min(int[] cost,int i,int[] dp)
-    {
-        if(i==cost.length-1)
+    public int ans(int[] nums,int[] dp,int i){
+        
+        if(i==nums.length-1)
         {
-            return cost[i];
+            return nums[i];
         }
-        if(i>=cost.length)
+        if(i>=nums.length)
         {
             return 0;
         }
+        
         if(dp[i]!=0)
         {
             return dp[i];
         }
+        int opt1=ans(nums,dp,i+1);
+        int opt2=ans(nums,dp,i+2);    
         
-        int opt1=min(cost,i+1,dp);
-        int opt2=min(cost,i+2,dp);
-        dp[i]=Math.min(opt1,opt2)+cost[i];
-        
+        dp[i]=Math.min(opt1,opt2)+nums[i];
         return dp[i];
     }
 }
