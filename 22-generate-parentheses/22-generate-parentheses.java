83a8811af4d7346e410c3f0ae0570aebc7842afb
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-07-14 05:24:29 -0700

    Time: 1 ms (97.51%), Space: 42.6 MB (85.81%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        
+       List<String> ans=new ArrayList<>();
+        
+     gen(n,0,0,ans,"");
+        return ans;
+    }
+    
+    public void gen(int max,int open,int close,List<String> ans,String s){
+        if(s.length()==2*max)
+        {
+            ans.add(s);
+            return;
+        }
+        
+        if(open<max)
+        {
+             gen(max,open+1,close,ans,s+"(");
+        }
+        
+         if(close<open)
+        {
+             gen(max,open,close+1,ans,s+")");
+        }
+    }
+}
