1781c604115655c4cb30238ee4652f906635c1f2
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-07-14 04:56:34 -0700

    Time: 1313 ms (5.01%), Space: 63.1 MB (5.76%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,33 +1,35 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         
-         List<List<Integer>> ans=new ArrayList<>();
+      List<List<Integer>> ans=new ArrayList<>();
         
         permutations(nums,ans,0);
-        
         return ans;
     }
    public static void permutations(int[] nums, List<List<Integer>> ans, int index)
           {
-              if(index==nums.length)
-              {
-                   List<Integer> ds=new ArrayList<>();
-                  for(int i=0; i<nums.length;i++)
-                  {
-                      ds.add(nums[i]);
-                  }
-                  if(!ans.contains(ds))
-                  {
-                  ans.add(new ArrayList<>(ds));
-              }
-              }
+       if(index==nums.length)
+       {
+           ArrayList<Integer> ds=new ArrayList<Integer>();
+           for(int i=0;i<nums.length;i++)
+           {
+               ds.add(nums[i]);
+           }
+           if(!ans.contains(new ArrayList<>(ds)))
+           {
+               ans.add(new ArrayList<>(ds));
+           }
+           return;
+       }
               
-              for(int i=index;i<nums.length;i++)
-              {
-                  swap(i,index,nums);
-                   permutations(nums,ans,index+1);
-                   swap(i,index,nums);
-              }
+       for(int i=index;i<nums.length;i++)
+       {
+           swap(i,index,nums);
+           
+                    permutations(nums,ans,index+1);
+                      swap(i,index,nums);
+
+       }
       }
       public static void swap(int i, int index, int[] nums)
       {
