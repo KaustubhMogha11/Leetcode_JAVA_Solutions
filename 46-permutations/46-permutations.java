4344170c393ad6b13cfd7fc5e88bbb137af2df70
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-07-14 04:16:18 -0700

    Time: 2 ms (76.41%), Space: 44.3 MB (72.75%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,73 +1,30 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         
-        
         List<List<Integer>> ans=new ArrayList<>();
-        // List<Integer> ds=new ArrayList<>();
-        // boolean freq[]=new boolean[nums.length];
+        ArrayList<Integer> ds=new ArrayList<>();
+        boolean freq[]=new boolean[nums.length];
         
-        permutations(nums,ans,0);
-        
+        permute(nums,ans,ds,freq);
         return ans;
     }
-      // public void permutations(int[] nums,List<Integer> ds, List<List<Integer>> ans, boolean[] freq){
-      //     if(ds.size()==nums.length){
-      //         ans.add(new ArrayList<>(ds));
-      //         return;
-      //     }
-      //     for(int i=0; i<nums.length;i++)
-      //     {
-      //         if(!freq[i])
-      //         {
-      //             freq[i]=true;
-      //             ds.add(nums[i]);
-      //              permutations(nums,ds,ans,freq);
-      //             ds.remove(ds.size()-1);
-      //             freq[i]=false;
-      //         }
-      //     }
     
-          public static void permutations(int[] nums, List<List<Integer>> ans, int index)
-          {
-//               if(index==nums.length)
-//               {
-//                    List<Integer> ds=new ArrayList<>();
-//                   for(int i=0; i<nums.length;i++)
-//                   {
-//                       ds.add(nums[i]);
-//                   }
-//                   ans.add(new ArrayList<>(ds));
-//               }
-              
-//               for(int i=index;i<nums.length;i++)
-//               {
-//                   swap(i,index,nums);
-//                    permutations(nums,ans,index+1);
-//                    swap(i,index,nums);
-//               }
-              
-              if(index==nums.length)
-              {
-                  List<Integer> ds=new ArrayList<>();
-                  for(int i=0;i<nums.length;i++)
-                  {
-                      ds.add(nums[i]);
-                  }
-                  ans.add(new ArrayList<>(ds));
-              }
-              
-              for(int i=index;i<nums.length;i++)
-              {
-                  swap(i,index,nums);
-                   permutations(nums,ans,index+1);
-                   swap(i,index,nums);
-              }
-      }
-      public static void swap(int i, int index, int[] nums)
-      {
-          int t=nums[i];
-          nums[i]=nums[index];
-          nums[index]=t;
-              
-      }
+    public void permute(int[] nums,List<List<Integer>> ans,ArrayList<Integer> ds,boolean[] freq){
+        if(ds.size()==nums.length)
+        {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!freq[i]){
+            freq[i]=true;
+            ds.add(nums[i]);
+            permute(nums,ans,ds,freq);
+            freq[i]=false;
+            ds.remove(ds.size()-1);
+            }
+        }
+    }
 }
