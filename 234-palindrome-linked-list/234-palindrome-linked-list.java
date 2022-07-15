530549b5367992021aebe3aafc7318dd691adefe
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-07-15 09:39:05 -0700

    Time: 7 ms (65.58%), Space: 97 MB (70.28%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -11,53 +11,49 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         
-        ListNode mid=mid(head);
-        ListNode reverseHead=reverse(mid);
-        ListNode head2=reverseHead;
+        if(head==null)
+            return false;
         
-        while(head!=null && head2!=null)
+        ListNode head1=head;
+        ListNode mid=mid(head);
+        ListNode head2=reverse(mid);
+        
+        while(head2!=null)
         {
-            if(head.val!=head2.val)
-            {
+            if(head1.val!=head2.val)
                 return false;
-            }
-            head=head.next;
+            
+            head1=head1.next;
             head2=head2.next;
         }
         
         return true;
-            
     }
     
     public ListNode mid(ListNode head){
         ListNode slow=head;
         ListNode fast=head;
         
-        while(fast!=null && fast.next!=null)
-        {
+        while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
         return slow;
     }
     
-    public ListNode reverse(ListNode head)
-    {
-          if (head == null) {
-            return head;
+    public ListNode reverse(ListNode head){
+        ListNode temp;
+        ListNode prev=null;
+        ListNode curr=head;
+        
+        while(curr!=null)
+        {
+            temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+        curr=temp;
         }
-        ListNode prev = null;
-        ListNode present = head;
-        ListNode next = present.next;
-
-        while (present != null) {
-            present.next = prev;
-            prev = present;
-            present = next;
-            if (next != null) {
-                next = next.next;
-            }
-        }
+        
         return prev;
     }
 }
