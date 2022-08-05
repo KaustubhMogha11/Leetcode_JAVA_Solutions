2243e155a618e62080b847952fd87be80e35ab5a
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-08-05 02:52:51 -0700

    Time: 0 ms (100.00%), Space: 39.3 MB (98.31%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,36 +1,28 @@
 class Solution {
+   
     public int combinationSum4(int[] nums, int target) {
-        
-        int dp[]=new int[target+1];
-         Arrays.fill(dp, -1);
-        return combinationSum4s(nums,target,dp);
+      int dp[]=new int[target+1];  
+           Arrays.fill(dp, -1);
+   return ans(nums,target,dp);
+       
     }
     
-    public int combinationSum4s(int[] nums, int target,int[] dp) {
-        
-        int count=0;
-      
+    public static int ans(int[] nums,int target,int[] dp)
+    {
+      int count=0;   
         if(target<0)
-        {
             return 0;
-        }
         if(target==0)
-        {
             return 1;
-        }
-          if(dp[target]!=-1)
-        {
+        
+        if(dp[target]!=-1)
             return dp[target];
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            count+=ans(nums,target-nums[i],dp);
         }
-        
-        
-       for(int num:nums)
-       {
-           count+=combinationSum4s(nums,target-num,dp);
-       }
-        
-      dp[target]=count;
-        
+        dp[target]=count;
         return count;
     }
 }
