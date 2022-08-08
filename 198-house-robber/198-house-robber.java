bf22412b3a329e5a1837de24c94968764282bf55
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-08-08 02:41:25 -0700

    Time: 0 ms (100.00%), Space: 41.8 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,16 +1,34 @@
 class Solution {
     public int rob(int[] nums) {
-
-         int dp[]=new int[nums.length+1];
-        dp[0]=0;
-        dp[1]=nums[0];
+       
+        int dp[]=new int[nums.length];
+//         dp[0]=0;
+//         dp[1]=nums[0];
         
-        for(int i=2;i<nums.length+1;i++)
-        {
-            dp[i]=Math.max(nums[i-1]+dp[i-2],dp[i-1]);
-        }
+//         for(int i=2;i<nums.length+1;i++)
+//         {
+//             dp[i]=Math.max(dp[i-2]+nums[i-1],dp[i-1]);
+//         }
         
-        return dp[nums.length];
+//         return dp[nums.length];
+        Arrays.fill(dp,-1);
         
-}
+        return ans(nums,dp,0);
+    }
+    
+    public static int ans(int[] nums,int[] dp,int i)
+    {
+        if(i==nums.length-1)
+            return nums[i];
+        if(i>=nums.length)
+            return 0;
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        int opt1=ans(nums,dp,i+1);
+        int opt2=ans(nums,dp,i+2)+nums[i];
+        
+        return dp[i]=Math.max(opt1,opt2);
+    }
 }
