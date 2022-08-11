153ae777bddc09774bed912076fea26ddf9c777d
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-08-11 09:31:19 -0700

    Time: 1 ms (62.14%), Space: 43.7 MB (69.53%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,50 +14,22 @@
  * }
  */
 class Solution {
-    
-//  public static int minimum(TreeNode root) {
-// 		if (root == null) {
-// 			return Integer.MAX_VALUE;
-// 		}
-// 		return Math.min(root.val, Math.min(minimum(root.left), minimum(root.right)));
-// 	}
-	
-	// public static int maximum(TreeNode root) {
-	// 	if (root == null) {
-	// 		return Integer.MIN_VALUE;
-	// 	}
-	// 	return Math.max(root.val, Math.max(maximum(root.left), maximum(root.right)));
-	// }
+    int prev=0;
     public boolean isValidBST(TreeNode root) {
-// if (root == null) {
-// 			return true;
-// 		}
-// 		int leftMax = maximum(root.left);
-// 		int rightMin = minimum(root.right);
-// 		if (root.val <= leftMax) {
-// 			return false;
-// 		}
-		
-// 		if (root.val >= rightMin) {
-// 			return false;
-// 		}
-// 		boolean isLeftBST = isValidBST(root.left);
-// 		boolean isRightBST = isValidBST(root.right);
-		
-//         return  (isLeftBST && isRightBST); 
-		
-           return isValidBSTUtil(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        
+        if(root==null)
+            return true;
+        
+      return ans(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
     
-    public boolean isValidBSTUtil(TreeNode root, long min, long max){
-        if(root == null){
+    public boolean ans(TreeNode root,long min,long max)
+    {
+        if(root==null)
             return true;
-        }
-        else if( root.val >= max || root.val <= min){
+        
+        if(root.val<=min || root.val>=max)
             return false;
-        }else{
-        return isValidBSTUtil(root.left, min, root.val) && isValidBSTUtil(root.right, root.val, max);  
+        else return ans(root.left,min,root.val)&&  ans(root.right, root.val, max);  
     }
 }
-}
- 
