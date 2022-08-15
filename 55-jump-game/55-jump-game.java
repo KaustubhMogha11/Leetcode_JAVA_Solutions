76a2dd872153092d51f3728e4b3b485d8fb3614c
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-08-15 02:02:49 -0700

    Time: 2 ms (93.25%), Space: 68.5 MB (27.48%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
-   
+        
         int reachable=0;
+        
         for(int i=0;i<nums.length;i++)
         {
             if(reachable<i)
-            {
                 return false;
-            }
             
             reachable=Math.max(reachable,i+nums[i]);
         }
         
         return true;
-      }
+    }
 }
