16145b0096131780caa0173b4d1a4a69e71f7edc
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-08-21 02:37:02 -0700

    Time: 10 ms (5.60%), Space: 42.1 MB (69.26%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,77 +1,29 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-//      int dp[][] = new int[obstacleGrid.length+1][obstacleGrid[0].length+1];
-//     for(int i=0;i<obstacleGrid.length;i++)
-//     {
-//         for(int j=0;j<obstacleGrid[0].length;j++)
-//         {
-//             if(i==0&&j==0&&obstacleGrid[i][j]==0)
-//                 dp[i+1][j+1] = 1;
-//             else if(obstacleGrid[i][j]==0)
-//                 dp[i+1][j+1] = dp[i][j+1]+dp[i+1][j];
-//         }
-//     }
-   
-//     return dp[obstacleGrid.length][obstacleGrid[0].length];
-  
+        
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
-        
-        int dp[][]=new int[m+1][n+1];
-        
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i==0 && j==0 && obstacleGrid[i][j]==0)
-                {
-                    dp[i+1][j+1]=1;
-                        
-                }else if(obstacleGrid[i][j]==0)
-                dp[i+1][j+1] = dp[i][j+1]+dp[i+1][j];
-            }
-        }
-        return dp[m][n];
-    }
-//         return  uniquePathsWithObstacles(obstacleGrid,0,0,dp);
-//     }
-//       public int uniquePathsWithObstacles(int[][] grid,int i, int j, int[][] dp)
-//       {
-//           int m=grid.length;
-//           int n=grid[0].length;
-         
-          
-//           if(dp[i][j]!=0)
-//           {
-//               return dp[i][j];
-//           }
-//            if(i>=m || j>=n)
-//           {
-//               dp[i][j]=0;
-//               return dp[i][j];
-              
-//           }
-//             if(grid[i][j]==1)
-//           {
-//               return 0;
-//           }
-          
-//           if(i==m-1 && j==n-1)
-//           {
-//               dp[i][j]=1;
-//               return dp[i][j];
-              
-//           }
-          
-         
-         
-              
-//           int left=uniquePathsWithObstacles(grid,i+1,j,dp);
-//           int right=uniquePathsWithObstacles(grid,i,j+1,dp);
-          
-//           dp[i][j]=left+right;
-          
-//           return dp[i][j];
-//       }
+        int dp[][]=new int[m][n];
     
+      return ans(0,0,m,n,obstacleGrid,dp);
+    }
+    
+    public static int ans(int i,int j,int m,int n,int[][] obstacleGrid,int dp[][])
+    {
+        if(i>=m || j>=n)
+            return 0;
+        if(obstacleGrid[i][j]==1)
+            return 0;
+      
+        if(dp[i][j]!=0)
+            return dp[i][j];
+        
+          if(i==m-1 && j==n-1)
+            return 1;
+        int left=ans(i,j+1,m,n,obstacleGrid,dp);
+        int right=ans(i+1,j,m,n,obstacleGrid,dp);
+        
+        return dp[i][j]=left+right;
+            
+    }
 }
