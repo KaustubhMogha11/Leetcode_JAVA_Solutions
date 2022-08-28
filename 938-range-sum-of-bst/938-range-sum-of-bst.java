2cad1e20b3c1c3baa77656e465be63c596ed5b4f
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-08-28 02:44:32 -0700

    Time: 1 ms (62.35%), Space: 49.9 MB (91.87%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.java
+++ 938-range-sum-of-bst/938-range-sum-of-bst.java
@@ -14,18 +14,18 @@
  * }
  */
 class Solution {
-    int ans=0;
+    int sum=0;
     public int rangeSumBST(TreeNode root, int low, int high) {
+        
         if(root==null)
             return 0;
-          if(root.val>=low && root.val<=high)
-          {
-              ans+=root.val;
-          }
+        
+        if(root.val>=low && root.val<=high)
+            sum+=root.val;
         
         rangeSumBST(root.left,low,high);
-              rangeSumBST(root.right,low,high);
+        rangeSumBST(root.right,low,high);
         
-        return ans;
+        return sum;
     }
 }
