0fecd54f3dc94ac50e45457249eed65febbba167
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-08-30 03:05:10 -0700

    Time: 2 ms (35.79%), Space: 42.7 MB (85.44%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -14,23 +14,47 @@
  * }
  */
 class Solution {
-     ArrayList<List<Integer>> list = new ArrayList<>();
-
+    List<List<Integer>> list=new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
+        
+//         if(root==null)
+//             return list;
+//         Queue<TreeNode> q=new LinkedList<>();
+//         q.add(root);
+//         while(!q.isEmpty())
+//         {
+//             int size=q.size();
+//             ArrayList<Integer> levelElement=new ArrayList<>();
+            
+//             for(int i=0;i<size;i++)
+//             {
+//                 TreeNode node=q.remove();
+//                 levelElement.add(node.val);
+//            if(node.left!=null) q.add(node.left);// these two lines are
+//                 if(node.right!=null) q.add(node.right);// used to add next level elements
+//             }
+//             list.add(levelElement);
+//         }
+//         return list;
+        
         if(root==null)
-        { 
-         return list;
-        }
-        Queue<TreeNode> q = new LinkedList<>();
+            return list;
+        
+       Queue<TreeNode> q=new LinkedList<>();
         q.add(root);
+        
         while(!q.isEmpty()){
-            int size = q.size(); // current size will have the total no of elements present at a given level in the tree
-            ArrayList<Integer> levelElement = new ArrayList<>(); // internal list that will store all the elements (where no. of elements at a given level are = size) at the given level 
-            for(int i =0;i<size;i++){
-                TreeNode node = q.remove();
+            int n=q.size();
+            ArrayList<Integer> levelElement=new ArrayList<>();
+            
+            for(int i=0;i<n;i++)
+            {
+                TreeNode node=q.remove();
                 levelElement.add(node.val);
-                if(node.left!=null) q.add(node.left);// these two lines are
-                if(node.right!=null) q.add(node.right);// used to add next level elements
+                if(node.left!=null)
+                    q.add(node.left);
+                if(node.right!=null)
+                    q.add(node.right);
             }
             list.add(levelElement);
         }
