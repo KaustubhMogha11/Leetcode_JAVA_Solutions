84d49c93da1e224e8cf6f4ce94c7ee87f0d339b3
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-09-12 14:33:24 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (19.66%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,32 +1,40 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int r=matrix.length;
-        int c=matrix[0].length;
-        List<Integer> list=new ArrayList<>();
+        
+        int row=matrix.length;
+        int col=matrix[0].length;
+        
         int rs=0;
-        int re=matrix.length-1;
         int cs=0;
-        int ce=matrix[0].length-1;
+        int re=row-1;
+        int ce=col-1;
         int count=0;
-        int total=r*c;
-     while(count<total) {
-			
-			for(int i=cs; count<total && i<=ce;i++) {
-				list.add(matrix[rs][i]);
-				count++;
-			}
-			rs++;
-			for(int i=rs; count<total && i<=re;i++) {
-				list.add(matrix[i][ce]);
-				count++;
-			}
-			ce--;
-			for(int i=ce;count<total && i>=cs;i--) {
-				list.add(matrix[re][i]);
-				count++;
-			}
-			re--;
-			for(int i=re;count<total && i>=rs;i--) {
+        int total=row*col;
+        List<Integer> list=new ArrayList<>();
+        while(count<row*col){
+            
+            for(int i=rs;count<total &&i<=ce;i++)
+            {
+                list.add(matrix[rs][i]);
+                count++;
+            }
+            rs++;
+            
+            for(int i=rs;count<total&& i<=re;i++)
+            {
+                list.add(matrix[i][ce]);
+                count++;
+            }
+            
+            ce--;
+            
+            for(int i=ce;count<total&& i>=cs;i--)
+            {
+                list.add(matrix[re][i]);
+                count++;
+            }
+            re--;
+       for(int i=re;count<total && i>=rs;i--) {
 				list.add(matrix[i][cs]);
 				count++;
 			}
