5dc0bddfb9a44a615e06dd874b31ad289262050b
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-09-29 16:29:59 +0530

    Time: 0 ms (100.00%), Space: 43 MB (63.07%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -18,6 +18,7 @@ class Solution {
         
         if(root==null)
             return 0;
-        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+        
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
     }
 }
